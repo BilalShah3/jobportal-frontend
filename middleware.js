5025ae872282a177de546fc3784e1fdb93fe9c57
Bilal Shah <2460@student.riphah.edu.pk>
2023-03-10 01:45:17 +0500

    first commit

---
+++ middleware.js
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse  } from "next/server";
+
+const allowedParams = [
+  "keyword",
+  "location",
+  "page",
+  "education",
+  "experience",
+  "salary",
+  "jobType",
+  "url",
+  "w",
+  "q",
+  "id",
+];
+
+export async function middleware(NextRequest) {
+  // const country = req.geo.country
+
+  const url = NextRequest.nextUrl;
+  console.log(url)
+  let changed = false;
+
+  url.searchParams.forEach((param, key) => {
+    if (!allowedParams.includes(key)) {
+      url.searchParams.delete(key);
+      changed = true;
+    }
+  });
+
+  if (changed) {
+    return NextResponse.redirect(url);
+  }
+}
